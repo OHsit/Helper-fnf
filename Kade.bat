76204fb4d7dace30d5880fb28fccab1397f31ebb
OHsit <79926004+OHsit@users.noreply.github.com>
2024-03-01 13:17:43 +0100

    nowe V2

--- Kade.bat
+++
@@ -1,9 +0,0 @@
-title Instalowanie Potrzebnych bibloitek do Kade Engine
-haxelib git faxe https://github.com/uhrobots/faxe
-haxelib git polymod https://github.com/larsiusprime/polymod.git
-haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
-haxelib install actuate
-haxelib git extension-webm https://github.com/KadeDev/extension-webm
-lime rebuild extension-webm windows
-haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
-exit
---
+++ Main.bat
@@ -0,0 +1,2 @@
+@echo off
+Title Wspomagacz do friday night funkin
