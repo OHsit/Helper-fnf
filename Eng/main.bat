2b13b29e3956d72b24a5f9fcaf02e5a792a846d4
Kapi6921 <79926004+OHsit@users.noreply.github.com>
2025-03-25 15:05:28 +0100

    optimize a scripts
    no more addtitonal scriptz

--- Eng/main.bat
+++ Eng/main.bat
@@ -34,13 +34,49 @@ echo "that command dosen't exists"
 goto select
 
 :Vanilla
-start Vanilla.bat
+color 02
+echo [HFNF] Installaing all important packages...
+haxelib --global install hmm
+haxelib --global run hmm setup
+hmm install
+haxelib run lime setup
+cls
+echo All done!
+pause
 exit
 
 :Kade
-start Kade.bat
+color 02
+echo [HFNF] Installaing all important packages...
+haxelib install lime 7.9.0
+haxelib install openfl
+haxelib install flixel
+haxelib install flixel-tools
+haxelib install flixel-ui
+haxelib install hscript
+haxelib install flixel-addons
+haxelib install actuate
+haxelib run lime setup
+haxelib run lime setup flixel
+haxelib run flixel-tools setup
+haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit.git
+haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
+haxelib git faxe https://github.com/uhrobots/faxe
+haxelib git polymod https://github.com/MasterEric/polymod.git
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git extension-webm https://github.com/KadeDev/extension-webm
+lime rebuild extension-webm windows
+cls
+echo All done!
+pause
 exit
 
 :Psych
-start Psych.bat
+color 02
+echo [HFNF] Installaing all important packages...
+curl -# -O https://download.visualstudio.microsoft.com/download/pr/3105fcfe-e771-41d6-9a1c-fc971e7d03a7/8eb13958dc429a6e6f7e0d6704d43a55f18d02a253608351b6bf6723ffdaf24e/vs_Community.exe
+vs_Community.exe --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK.19041 -p
+cls
+echo All done!
+pause
 exit
--- Pl/main.bat
+++ Pl/main.bat
@@ -34,13 +34,51 @@ echo "Nie ma takiej komendy"
 goto select
 
 :Vanilla
-start Vanilla.bat
+color 02
+echo [WSFNF] Installowanie potrzebnych bibliotek...
+haxelib --global install hmm
+haxelib --global run hmm setup
+hmm install
+haxelib run lime setup
+cls
+echo Wszystko zainstallowane!
+echo Mozna spokojne zamknonć.
+pause
 exit
 
 :Kade
-start Kade.bat
+color 02
+echo [WSFNF] Installowanie potrzebnych bibliotek...
+haxelib install lime 7.9.0
+haxelib install openfl
+haxelib install flixel
+haxelib install flixel-tools
+haxelib install flixel-ui
+haxelib install hscript
+haxelib install flixel-addons
+haxelib install actuate
+haxelib run lime setup
+haxelib run lime setup flixel
+haxelib run flixel-tools setup
+haxelib git linc_luajit https://github.com/nebulazorua/linc_luajit.git
+haxelib git hxvm-luajit https://github.com/nebulazorua/hxvm-luajit
+haxelib git faxe https://github.com/uhrobots/faxe
+haxelib git polymod https://github.com/MasterEric/polymod.git
+haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc
+haxelib git extension-webm https://github.com/KadeDev/extension-webm
+lime rebuild extension-webm windows
+cls
+echo Wszystko zainstallowane!
+pause
 exit
 
 :Psych
-start Psych.bat
+color 02
+echo [WSFNF] Installowanie potrzebnych bibliotek...
+curl -# -O https://download.visualstudio.microsoft.com/download/pr/3105fcfe-e771-41d6-9a1c-fc971e7d03a7/8eb13958dc429a6e6f7e0d6704d43a55f18d02a253608351b6bf6723ffdaf24e/vs_Community.exe
+vs_Community.exe --add Microsoft.VisualStudio.Component.VC.Tools.x86.x64 --add Microsoft.VisualStudio.Component.Windows10SDK.19041 -p
+cls
+echo Wszystko zainstallowane!
+echo Mozna spokojne zamknonć.
+pause
 exit
